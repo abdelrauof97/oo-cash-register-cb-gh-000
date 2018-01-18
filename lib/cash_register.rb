dbc04663ec1ea7f8ae196ad3c138f3404c206f23
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-18 16:22:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,8 @@ class CashRegister
   def add_item(title,price,quantity=1)
     self.total += price*quantity
     quantity.times do
-      
-    }
+      @items << title
+    end
   end
   
   def apply_discount(employee_discount=20)
