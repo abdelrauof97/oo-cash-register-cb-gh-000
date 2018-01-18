fa9c4f1d15492c4a59cfc6fbb77ebb270559e2db
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-18 16:26:59 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 require "pry"
 class CashRegister 
   
-  attr_accessor :discount, :total, :items
+  attr_accessor :discount, :total
+  attr_reader :items
   
   def initialize(discount=0)
     @items = []
